dc51c9933c48b99b32d69a41960fc3198e13caa4
DayyalaAvinash <dayyalaavinash1727@gmail.com>
2026-05-13 10:04:55 +0530

    Update ContactModuleTest.java

--- src/test/java/Contact_Module/ContactModuleTest.java
+++ src/test/java/Contact_Module/ContactModuleTest.java
@@ -27,6 +27,9 @@ import vtiger_crm.HomepagePomPage;
 import vtiger_crm.Organizations_PomPage;
 import vtiger_crm.organization_InformationPomPage;
 @Listeners(ListenerUtility.Listeners.class)
+	/**
+	*This Class Is Worked On Contact Module
+	*/
 public class ContactModuleTest extends BaseClass {
 
 	
