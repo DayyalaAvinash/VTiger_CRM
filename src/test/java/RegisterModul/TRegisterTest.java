34fa7826b6438fd64cbaaa624e7379eb5b42b500
AVINASH D <AVINASH D@AVINASH>
2026-05-13 11:03:52 +0530

    Ravi First Push

---
+++ src/test/java/RegisterModul/TRegisterTest.java
@@ -0,0 +1,12 @@
+package RegisterModul;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TRegisterTest {
+
+	@Test
+	public void register() {
+		Reporter.log("Register");
+	}
+}
